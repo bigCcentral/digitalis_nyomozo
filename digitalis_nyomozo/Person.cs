f749c934bdd17c5fd5df6187276b38260e89c495
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 14:35:39 +0100

    Person_1

--- digitalis_nyomozo/Person.cs
+++ digitalis_nyomozo/Person.cs
@@ -8,5 +8,19 @@ namespace digitalis_nyomozo
 {
 	internal class Person
 	{
+		private string nev;
+		private int eletkor;
+		private string megjegyzes;
+
+		public Person(string nev, int eletkor, string megjegyzes)
+		{
+			this.nev = nev;
+			this.eletkor = eletkor;
+			this.megjegyzes = megjegyzes;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Eletkor { get => eletkor; set => eletkor = value; }
+		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
 	}
 }
