8ab67a7bb3a69a3477fa98c10f0d01a8aebfe776
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 14:39:24 +0100

    Suspect_1

--- digitalis_nyomozo/Suspect.cs
+++ digitalis_nyomozo/Suspect.cs
@@ -8,5 +8,19 @@ namespace digitalis_nyomozo
 {
 	internal class Suspect
 	{
+		private Person person;
+		private int gyanusitottsagi_szint;
+		private string statusz;
+
+		public Suspect(Person person, int gyanusitottsagi_szint, string statusz)
+		{
+			this.person = person;
+			this.gyanusitottsagi_szint = gyanusitottsagi_szint;
+			this.statusz = statusz;
+		}
+
+		public int Gyanusitottsagi_szint { get => gyanusitottsagi_szint; set => gyanusitottsagi_szint = value; }
+		public string Statusz { get => statusz; set => statusz = value; }
+		internal Person Person { get => person; set => person = value; }
 	}
 }
