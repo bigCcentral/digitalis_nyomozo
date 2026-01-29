d2e4203feed4d01aedf841d6985f192803c78e40
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 14:46:31 +0100

    TimelineEvent_1

--- digitalis_nyomozo/TimelineEvent.cs
+++ digitalis_nyomozo/TimelineEvent.cs
@@ -8,5 +8,16 @@ namespace digitalis_nyomozo
 {
 	internal class TimelineEvent
 	{
+		private DateTime datum;
+		private string esemeny_leiras;
+
+		public TimelineEvent(DateTime datum, string esemeny_leiras)
+		{
+			this.datum = datum;
+			this.esemeny_leiras = esemeny_leiras;
+		}
+
+		public DateTime Datum { get => datum; set => datum = value; }
+		public string Esemeny_leiras { get => esemeny_leiras; set => esemeny_leiras = value; }
 	}
 }
