f8791ebeea37bf921063dbd51a6f716e842da62b
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 14:40:48 +0100

    Witness_1

--- digitalis_nyomozo/Witness.cs
+++ digitalis_nyomozo/Witness.cs
@@ -8,5 +8,16 @@ namespace digitalis_nyomozo
 {
 	internal class Witness
 	{
+		private Person vallomas_szoveg;
+		private int vallomas_datum;
+
+		public Witness(Person vallomas_szoveg, int vallomas_datum)
+		{
+			this.vallomas_szoveg = vallomas_szoveg;
+			this.vallomas_datum = vallomas_datum;
+		}
+
+		public int Vallomas_datum { get => vallomas_datum; set => vallomas_datum = value; }
+		internal Person Vallomas_szoveg { get => vallomas_szoveg; set => vallomas_szoveg = value; }
 	}
 }
