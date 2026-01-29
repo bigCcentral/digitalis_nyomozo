f1672b19292cad1c88b9b23776b4fe8287feb138
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 13:52:23 +0100

    Add project files.

---
+++ digitalis_nyomozo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace digitalis_nyomozo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
