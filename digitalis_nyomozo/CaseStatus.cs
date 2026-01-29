08d84b578743dd1ef2a7fffc0dc8083ed28325ea
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 14:32:38 +0100

    CaseStatus_1

--- digitalis_nyomozo/CaseStatus.cs
+++ digitalis_nyomozo/CaseStatus.cs
@@ -8,5 +8,13 @@ namespace digitalis_nyomozo
 {
 	internal class CaseStatus
 	{
+		private string aktualis_statusz;
+
+		public CaseStatus(string aktualis_statusz)
+		{
+			this.aktualis_statusz = aktualis_statusz;
+		}
+
+		public string Aktualis_statusz { get => aktualis_statusz; set => aktualis_statusz = value; }
 	}
 }
