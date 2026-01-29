f038b625a14ee854cf41078ee387a4cb7c0488b7
juhasz <juhasz.cecilia.piroska@petrik.hu>
2026-01-29 14:30:30 +0100

    case_1

--- digitalis_nyomozo/Case.cs
+++ digitalis_nyomozo/Case.cs
@@ -8,6 +8,22 @@ namespace digitalis_nyomozo
 {
 	internal class Case
 	{
+		private int azonosito;
+		private string cim;
+		private string leiras;
+		private string allapot;
 
+		public Case(int azonosito, string cim, string leiras, string allapot)
+		{
+			this.azonosito = azonosito;
+			this.cim = cim;
+			this.leiras = leiras;
+			this.allapot = allapot;
+		}
+
+		public int Azonosito { get => azonosito; set => azonosito = value; }
+		public string Cim { get => cim; set => cim = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+		public string Allapot { get => allapot; set => allapot = value; }
 	}
 }
