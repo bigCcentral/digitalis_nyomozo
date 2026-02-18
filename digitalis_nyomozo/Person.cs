d2ce8cd6757f591a18c44bdef34e78fcf864b0ce
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:41:52 +0100

    Person_proba

--- digitalis_nyomozo/Person.cs
+++ digitalis_nyomozo/Person.cs
@@ -1,26 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace digitalis_nyomozo
 {
-	internal class Person
-	{
-		private string nev;
-		private int eletkor;
-		private string megjegyzes;
+    internal class Person
+    {
+        private string nev;
+        private int eletkor;
+        private string megjegyzes;
 
-		public Person(string nev, int eletkor, string megjegyzes)
-		{
-			this.nev = nev;
-			this.eletkor = eletkor;
-			this.megjegyzes = megjegyzes;
-		}
+        public Person(string nev, int eletkor, string megjegyzes)
+        {
+            Nev = nev;
+            Eletkor = eletkor;
+            Megjegyzes = megjegyzes;
+        }
 
-		public string Nev { get => nev; set => nev = value; }
-		public int Eletkor { get => eletkor; set => eletkor = value; }
-		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
-	}
+        public string Nev
+        {
+            get => nev;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("A név nem lehet üres.");
+                nev = value;
+            }
+        }
+
+        public int Eletkor
+        {
+            get => eletkor;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Az életkor nem lehet negatív.");//AI segitett benne 
+                eletkor = value;
+            }
+        }
+
+        public string Megjegyzes
+        {
+            get => megjegyzes;
+            set => megjegyzes = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev} ({Eletkor} év) - {Megjegyzes}";
+        }
+    }
 }
