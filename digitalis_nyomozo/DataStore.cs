ce53aaad828c822b3638f145c666b9ed2644d455
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:18:02 +0100

    DataStore_vegleges

--- digitalis_nyomozo/DataStore.cs
+++ digitalis_nyomozo/DataStore.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace digitalis_nyomozo
 {
-	internal class DataStore
-	{
-	}
+    internal class DataStore
+    {
+        private List<User> users;
+        private List<Case> cases;
+        private List<Person> persons;
+        private List<Evidence> evidences;
+
+        public DataStore()
+        {
+            users =  new List<User>();
+            cases =new List<Case>();
+            persons =new List<Person>();
+            evidences =new List<Evidence>();
+        }
+
+        public List<User> Users => users;
+        public List<Case> Cases => cases;
+        public List<Person> Persons => persons;
+        public List<Evidence> Evidences => evidences;
+    }
 }
