d0278bdc00890f8192643eb4b56b31fcba8b6313
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:00:32 +0100

    case_vegleges

--- digitalis_nyomozo/Case.cs
+++ digitalis_nyomozo/Case.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace digitalis_nyomozo
 {
-	internal class Case
-	{
-		private int azonosito;
-		private string cim;
-		private string leiras;
-		private string allapot;
+    internal class Case
+    {
+        private int azonosito;
+        private string cim;
+        private string leiras;
+        private CaseStatus allapot;
 
-		public Case(int azonosito, string cim, string leiras, string allapot)
-		{
-			this.azonosito = azonosito;
-			this.cim = cim;
-			this.leiras = leiras;
-			this.allapot = allapot;
-		}
+        private List<Person> szemelyek;
+        private List<Evidence> bizonyitekok;
+        private List<TimelineEvent> idovonal;
 
-		public int Azonosito { get => azonosito; set => azonosito = value; }
-		public string Cim { get => cim; set => cim = value; }
-		public string Leiras { get => leiras; set => leiras = value; }
-		public string Allapot { get => allapot; set => allapot = value; }
-	}
+        public Case(int azonosito, string cim, string leiras, CaseStatus allapot)
+        {
+            this.azonosito = azonosito;
+            this.cim = cim;
+            this.leiras = leiras;
+            this.allapot = allapot;
+
+            szemelyek =new List<Person>();
+            bizonyitekok =new List<Evidence>();
+            idovonal =new List<TimelineEvent>();
+        }
+
+        public int Azonosito { get => azonosito; set => azonosito = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public string Leiras { get => leiras; set => leiras = value; }
+        public CaseStatus Allapot { get => allapot; set => allapot = value; }
+
+        public List<Person> Szemelyek => szemelyek;
+        public List<Evidence> Bizonyitekok => bizonyitekok;
+        public List<TimelineEvent> Idovonal => idovonal;
+    }
 }
