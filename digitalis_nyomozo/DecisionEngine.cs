e21193dbf2c326a299acf8a4f41f09d0fd48f9ed
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:21:23 +0100

    Suspect_vegleges

--- digitalis_nyomozo/DecisionEngine.cs
+++ digitalis_nyomozo/DecisionEngine.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace digitalis_nyomozo
 {
-	internal class DecisionEngine
-	{
-	}
+    internal class DecisionEngine
+    {
+        public void Ertekeles(Suspect gyanusitott, List<Evidence> bizonyitekok)
+        {
+            int osszeg = 0;
+
+            foreach (var b in bizonyitekok)
+            {
+                osszeg += b.Megbizhatosag;
+            }
+
+            gyanusitott.GyanusitottsagiSzint += osszeg * 5;
+
+            Console.WriteLine($"Új gyanúsítottsági szint: {gyanusitott.GyanusitottsagiSzint}");
+
+            if (gyanusitott.GyanusitottsagiSzint >= 70)
+            {
+                Console.WriteLine("FIGYELEM! A gyanúsított elérte a veszélyes küszöbértéket!");
+            }
+        }
+    }
 }
--- digitalis_nyomozo/Suspect.cs
+++ digitalis_nyomozo/Suspect.cs
@@ -1,26 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using digitalis_nyomozo;
 
-namespace digitalis_nyomozo
+internal class Suspect
 {
-	internal class Suspect
-	{
-		private Person person;
-		private int gyanusitottsagi_szint;
-		private string statusz;
+    private Person person;
+    private int gyanusitottsagi_szint;
+    private string statusz;
 
-		public Suspect(Person person, int gyanusitottsagi_szint, string statusz)
-		{
-			this.person = person;
-			this.gyanusitottsagi_szint = gyanusitottsagi_szint;
-			this.statusz = statusz;
-		}
+    public Suspect(Person person, int gyanusitottsagi_szint, string statusz)
+    {
+        this.person = person;
+        Gyanusitottsagi_szint = gyanusitottsagi_szint;
+        Statusz = statusz;
+    }
 
-		public int Gyanusitottsagi_szint { get => gyanusitottsagi_szint; set => gyanusitottsagi_szint = value; }
-		public string Statusz { get => statusz; set => statusz = value; }
-		internal Person Person { get => person; set => person = value; }
-	}
+    public int Gyanusitottsagi_szint
+    {
+        get => gyanusitottsagi_szint;
+        set
+        {
+            if (value < 0)
+                gyanusitottsagi_szint = 0;
+            else if (value > 100)
+                gyanusitottsagi_szint = 100;
+            else
+                gyanusitottsagi_szint = value;
+        }
+    }
+
+    public string Statusz
+    {
+        get => statusz;
+        set
+        {
+            if (value == "Szabad" || value == "Megfigyelt" || value == "Orizetben")
+                statusz = value;
+            else
+                Console.WriteLine("Érvénytelen státusz!");
+        }
+    }
+
+    internal Person Person { get => person; set => person = value; }
 }
