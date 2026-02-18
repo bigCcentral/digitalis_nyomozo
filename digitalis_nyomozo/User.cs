83c217a6c8c6cf3ce13d199f7e65e43a8fcf0d7f
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:46:49 +0100

    User_vegleges

--- digitalis_nyomozo/User.cs
+++ digitalis_nyomozo/User.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace digitalis_nyomozo
+﻿namespace digitalis_nyomozo
 {
-	internal class User
-	{
-		private string nev;
-		private int azonosito;
-		private string szerepkor;
-
-		public User(string nev, int azonosito, string szerepkor)
-		{
-			this.nev = nev;
-			this.azonosito = azonosito;
-			this.szerepkor = szerepkor;
-		}
-
-		public string Nev { get => nev; set => nev = value; }
-		public int Azonosito { get => azonosito; set => azonosito = value; }
-		public string Szerepkor { get => szerepkor; set => szerepkor = value; }
-	}
+    internal class User
+    {
+        private string nev;
+        private int azonosito;
+        private string szerepkor;
+        public User(string nev, int azonosito, string szerepkor)
+        {
+            this.nev = nev;
+            this.azonosito = azonosito;
+            this.szerepkor = szerepkor;
+        }
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+        public int Azonosito
+        {
+            get { return azonosito; }
+            set { azonosito = value; }
+        }
+        public string Szerepkor
+        {
+            get { return szerepkor; }
+            set { szerepkor = value; }
+        }
+        public override string ToString()
+        {
+            return azonosito + " _ " + nev + " (" + szerepkor + ")";
+        }
+    }
 }
