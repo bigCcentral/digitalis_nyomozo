f56baaa749dcfa6d37b2d67e00ad31c00593f110
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:05:42 +0100

    caseManager_vegleges

--- digitalis_nyomozo/CaseManager.cs
+++ digitalis_nyomozo/CaseManager.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace digitalis_nyomozo
 {
-	internal class CaseManager
-	{
-	}
+    internal class CaseManager
+    {
+        private List<Case> cases;
+
+        public CaseManager()
+        {
+            cases = new List<Case>();
+        }
+
+        public void UjUgyLetrehozasa(Case ujCase)
+        {
+            cases.Add(ujCase);
+            Console.WriteLine("Ügy sikeresen letrehozva");
+        }
+
+        public void UgyekListazasa()
+        {
+            if (cases.Count == 0)
+            {
+                Console.WriteLine("Nincs rögzített ügy");
+                return;
+            }
+        }
+
+    }
 }
