f18c11d2010323f5f57e358d29d1de1b1c0ca995
bigCcentral <juhasz.cecilia.piroska@petrik.hu>
2026-02-18 18:48:32 +0100

    Witness_proba

--- digitalis_nyomozo/Witness.cs
+++ digitalis_nyomozo/Witness.cs
@@ -1,23 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace digitalis_nyomozo
 {
-	internal class Witness
-	{
-		private Person vallomas_szoveg;
-		private DateTime vallomas_datum;
+    internal class Witness
+    {
+        private Person szemely;
+        private string vallomasSzoveg;
+        private DateTime vallomasDatum;
 
-		public Witness(Person vallomas_szoveg, DateTime vallomas_datum)
-		{
-			this.vallomas_szoveg = vallomas_szoveg;
-			this.vallomas_datum = vallomas_datum;
-		}
+        public Witness(Person szemely, string vallomasSzoveg, DateTime vallomasDatum)
+        {
+            this.szemely = szemely;
+            this.vallomasSzoveg = vallomasSzoveg;
+            this.vallomasDatum = vallomasDatum;
+        }
 
-		public DateTime Vallomas_datum { get => vallomas_datum; set => vallomas_datum = value; }
-		internal Person Vallomas_szoveg { get => vallomas_szoveg; set => vallomas_szoveg = value; }
-	}
+        public Person Szemely
+        {
+            get { return szemely; }
+            set { szemely = value; }
+        }
+
+        public string VallomasSzoveg
+        {
+            get { return vallomasSzoveg; }
+            set { vallomasSzoveg = value; }
+        }
+
+        public DateTime VallomasDatum
+        {
+            get { return vallomasDatum; }
+            set { vallomasDatum = value; }
+        }
+
+        public override string ToString()
+        {
+            return szemely.Nev + " - Vallomás: " + vallomasSzoveg +
+                   " (" + vallomasDatum.ToShortDateString() + ")";
+        }
+    }
 }
